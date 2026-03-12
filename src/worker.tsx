d4e6c9f89b493d4496e3a67df40f1871586febc3
Aleksander Jenssen <aleksander-j29@hotmail.com>
2026-03-12 01:50:51 +0100

    Fix

--- src/worker.tsx
+++ src/worker.tsx
@@ -1,5 +1,6 @@
 import { render, route } from "rwsdk/router";
 import { defineApp } from "rwsdk/worker";
+import { syncedStateRoutes, SyncedStateServer } from "rwsdk/use-synced-state/worker";
 
 import { Document } from "@/app/document";
 import { setCommonHeaders } from "@/app/headers";
@@ -7,8 +8,15 @@ import { Home } from "@/app/pages/home";
 
 export type AppContext = {};
 
+type SyncedStateEnv = Cloudflare.Env & {
+  SYNCED_STATE: DurableObjectNamespace<SyncedStateServer>;
+};
+
+export { SyncedStateServer as SyncedState };
+
 export default defineApp([
   setCommonHeaders(),
+  ...syncedStateRoutes((env) => (env as SyncedStateEnv).SYNCED_STATE),
   ({ ctx }) => {
     // setup ctx here
     ctx;
